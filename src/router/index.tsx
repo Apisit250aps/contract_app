aa3effd9d26c0709045df8e49df358dccd775011
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-09-05 00:00:14 +0700

    new router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,33 +1,63 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, RouteObject } from "react-router-dom"
 import Index from "../pages/Index"
 import LoginPage from "../pages/auth/LoginPage"
 import RegisterPage from "../pages/auth/RegisterPage"
 import ProjectPage from "../pages/projects/ProjectPage"
+import EmployeePage from "../pages/employee/EmployeePage"
 import DrawerLayout from "../components/layouts/DrawerLayout"
+import { ReactNode } from "react"
 
-const router = createBrowserRouter([
+// Extend NonIndexRouteObject and IndexRouteObject to include 'name' property
+declare module "react-router-dom" {
+  interface NonIndexRouteObject {
+    name?: string
+    icon?: ReactNode | null
+  }
+  interface IndexRouteObject {
+    name?: string
+    icon?: ReactNode | null
+  }
+}
+
+export const IndexRouter: RouteObject[] = [
   {
     path: "/",
-    element: <DrawerLayout />, // ใช้ Layout
+    name: "home",
+    element: <DrawerLayout />,
     children: [
       {
         path: "/",
-        element: <Index />
+        element: <Index />,
+        name: "index"
       },
       {
         path: "/project",
         element: <ProjectPage />,
+        name: "project"
+      },
+      {
+        path: "/employee",
+        element: <EmployeePage />,
+        name: "employee"
       }
     ]
-  },
-  {
-    path: "/auth/login",
-    element: <LoginPage />
-  },
-  {
-    path: "/auth/register",
-    element: <RegisterPage />
   }
-])
+]
+
+export const routerObjects: RouteObject[] = [
+  ...IndexRouter,
+  {
+    path: "auth/login",
+    element: <LoginPage />,
+    name: "login"
+  },
+  {
+    path: "auth/register",
+    element: <RegisterPage />,
+    name: "register"
+  }
+]
+
+const router = createBrowserRouter(routerObjects)
 
 export default router
