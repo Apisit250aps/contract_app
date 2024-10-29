77a34df8fb5259e6d143a27f3b94a4a95513684a
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-10-29 22:38:10 +0700

    login page, form

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,50 +1,22 @@
 import { FC } from "react"
+import LoginForm from "../../components/form/auth/LoginForm"
+import { Link } from "react-router-dom"
 
 const LoginPage: FC = () => {
   return (
     <>
       <div className="text-center lg:text-left">
-        <h1 className="text-5xl font-bold">Login now!</h1>
+        <h1 className="text-5xl font-bold">Staff Login Portal</h1>
         <p className="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
+          Welcome to the internal management system for staff and
+          administrators. Please sign in with your assigned account to manage
+          and track your work assignments.
         </p>
+        <Link to={"/auth/register"} className=" link">
+          No account? Contact system administrator
+        </Link>
       </div>
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
-          </div>
-        </form>
-      </div>
+      <LoginForm />
     </>
   )
 }
